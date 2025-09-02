6f99e45d6e766794b1a2ec64c7b6a60318b0cf8a
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2025-09-02 21:03:56 +0530

    Person A made some changes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <div>
       <p>Hii Guys!!</p>
-      <Button variant="default">Button</Button>
+      <div>
+        About
+        AI Trip Planner is an intelligent tool that creates personalized travel plans and itineraries based on user preferences.
+      </div>
+      <Button variant="destructive">Button</Button>
     </div>
   );
 }
